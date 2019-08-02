f2e7ebfdc1d31aa692eed2ad15ef029fefdbbeb6
Andrey Paramonov <aparamon@my.centennialcollege.ca>
2019-08-01 22:16:52 -0400

    BEFORE: select form edition

--- Store_App/SelectForm.Designer.cs
+++ Store_App/SelectForm.Designer.cs
@@ -396,7 +396,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(830, 406);
+            this.ClientSize = new System.Drawing.Size(830, 414);
             this.Controls.Add(this.dataGridTable);
             this.Controls.Add(this.label1);
             this.Name = "SelectForm";
